4c8ad80986ad1f3b0c30d3914744081d837c1477
algalopez <algalopez85@gmail.com>
2019-11-17 00:30:54 +0100

    #20 Move properties to docker environments
    Move properties to docker environments
    Remove comments
    Check if database docker has started correctly
    Move token to travis
    Change docker image
    Add information to readme

---
+++ docker/script/WaitForDatabase.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+trie=0
+numberOfTries=30
+secondsBetweenTries=1
+
+user=$(docker exec db-docker bash -c 'echo "$MARIADB_USER"' 2> /dev/null)
+pass=$(docker exec db-docker bash -c 'echo "$MARIADB_PASSWORD"' 2> /dev/null)
+url=$(docker exec db-docker bash -c 'echo "$DATABASE_URL"' 2> /dev/null)
+port=$(docker exec db-docker bash -c 'echo "$DATABASE_PORT"' 2> /dev/null)
+
+while [ $((++trie)) -le $numberOfTries ] && [ $status!="2" ]
+do
+
+  status=$(echo "show databases;" | mysql -u $user -p$pass -h$url -P$port 2> /dev/null | grep tunturi)
+
+  if [[ $status == "tunturi" ]]
+  then
+    echo Database is ready
+    exit 0
+  else
+    echo 'Waiting for database' $trie
+    sleep $secondsBetweenTries
+  fi
+
+done
+
+exit 1
